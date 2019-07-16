363e80a5fc87b96cf7f29ed06b931c94d7805c4e
Warren Rawlings <rawlings.warren@gmail.com>
2019-07-15 21:46:20 -0400

    had an error undefined for my main attr

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,29 +5,41 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
-
+function CuboidMaker(attr) {
+    this.lengthA = attr.lengthA;
+    this.widthA = attr.widthA;
+    this.heightA = attr.heightA;
+}
 
 /* == Step 2: Volume Method ==
   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   
   Formula for cuboid volume: length * width * height
 */
-
+CuboidMaker.prototype.volume = function() {
+    console.log(lengthA * widthA * heightA);
+};
 
 /* == Step 3: Surface Area Method ==
   Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
-
+CuboidMaker.prototype.surfaceArea = function() {
+    console.log(2 * (lengthA * widthA + lengthA * heightA + widthA * heightA));
+};
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
+const cuboid = new CuboidMaker({
+    lengthA: 4,
+    widthA: 5,
+    heightA: 5,
+});
+console.log(cuboid); // checking to see if this worked. It did.
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
-
-
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
