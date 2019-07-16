44904a5746671d735f5521f815e31622a576bb10
Warren Rawlings <rawlings.warren@gmail.com>
2019-07-15 20:45:08 -0400

    completed funciton

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,7 +7,7 @@
  * In the body of the function return the callback with the two parameters that you created
  */
 function consume(name1, name2, callback) {
-    callback(`${name1} please meet ${name2}`);
+    console.log(callback(name1, name2));
 }
 
 
@@ -16,20 +16,22 @@ function consume(name1, name2, callback) {
  * Create a function named multiply that returns the product of two numbers 
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
-let add = function(num1, num2) {
+function add(num1, num2) {
     return num1 + num2;
 }
-let multiply = function(num1, num2) {
+
+function multiply(num1, num2) {
     return num1 * num2;
 }
-let greeting = function(firstName, lastName) {
+
+function greeting(firstName, lastName) {
     return `Hello ${firstName} ${lastName}, nice to meet you!`
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2, 2, add); // 4
+consume(10, 16, multiply); // 160
+consume("Mary", "Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -37,6 +39,7 @@ let greeting = function(firstName, lastName) {
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+/*the concept of  Closure gives access to an outer functions scope from an inner function. Closure is an extension to the conecpt of scope but not a replacement. It is not quite inheritance except in the world of nested functions like the example below.*/
 
 
 const external = "I'm outside the function";
